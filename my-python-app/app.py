41746401bc3a74c57870021eb4d001ea2a02f926
manpreetiitm <manpreetiitm@gmail.com>
2024-08-01 12:12:18 +0530

    Updating source code and dockerfile

--- my-python-app/app.py
+++ my-python-app/app.py
@@ -1,18 +1,27 @@
-from flask import Flask
-import psycopg2
+from flask import Flask, jsonify
+import random
 import os
 
 app = Flask(__name__)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+metrics = {
+    "invocations": 0,
+    "http_status_codes": {
+        "200": 0,
+        "500": 0
+    }
+}
 
 @app.route('/')
 def home():
-    return "Hello, Flask is running!"
+    metrics["invocations"] += 1
+    status_code = random.choice([200, 500])
+    metrics["http_status_codes"][str(status_code)] += 1
+    return jsonify({"message": "Hello, Flask!", "status_code": status_code}), status_code
 
-def connect_db():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+@app.route('/metrics')
+def prometheus_metrics():
+    return jsonify(metrics)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
